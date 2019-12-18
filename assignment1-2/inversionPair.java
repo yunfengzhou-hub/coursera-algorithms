a5cfb42d3da9292e069f7bfbe325b3abceaf567c
yunfeng zhou <yunfengz@andrew.cmu.edu>
2019-12-18 15:27:30 -0500

    read int from file

--- assignment1-2/inversionPair.java
+++ assignment1-2/inversionPair.java
@@ -1,5 +1,19 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class inversionPair {
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static void main(String[] args) throws Exception {
+        File file = new File("C:\\mydata\\coursera\\algorithm\\coursera-algorithms\\assignment1-2\\IntegerArray.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file)); 
+        List<String> oriArray = new ArrayList<String>();
+        String tmpInput;
+        while((tmpInput = br.readLine()) != null){
+            oriArray.add(tmpInput);
+        }
+        System.out.println(oriArray.size());
+        br.close();
     }
 }
