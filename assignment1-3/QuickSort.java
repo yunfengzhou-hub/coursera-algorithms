98b8a68e848174873442229392ddd2d6aec8ab74
yunfeng zhou <yunfengz@andrew.cmu.edu>
2019-12-19 11:17:52 -0500

    try 3 pivot selecting strategy

--- assignment1-3/QuickSort.java
+++ assignment1-3/QuickSort.java
@@ -10,6 +10,16 @@ public class QuickSort {
     static List<Integer> oriArray;
     static BigInteger compCount = new BigInteger("0");
     private static void choosePivot(int start, int finish){
+        // Collections.swap(oriArray,start,finish-1); /* choose the last as pivot */
+        int a1 = oriArray.get(start);
+        int a2 = oriArray.get((start+finish-1)/2);
+        int a3 = oriArray.get(finish-1);
+        if((a1<a2 && a2<a3)||(a1>a2 && a2>a3)){
+            Collections.swap(oriArray,start,(start+finish-1)/2); /* choose the middle of three as pivot */
+        }
+        else if((a1<a3 && a3<a2)||(a1>a3 && a3>a2)){
+            Collections.swap(oriArray,start,finish-1); /* choose the middle of three as pivot */
+        }
         return ;
     }
     private static void myIteration(int start, int finish){
@@ -31,7 +41,7 @@ public class QuickSort {
         return ;
     }
     public static void main(String[] args) throws Exception {
-        File file = new File("IntegerArray.txt");
+        File file = new File("QuickSort.txt");
         BufferedReader br = new BufferedReader(new FileReader(file)); 
         oriArray = new ArrayList<Integer>();
         String tmpInput;
@@ -41,6 +51,13 @@ public class QuickSort {
         myIteration(0,oriArray.size());
         System.out.println(oriArray.size());
         System.out.println(oriArray.subList(0, 10));
+        System.out.println(compCount);
+        /*
+         * number of comparision:
+         * always use first as pivot:   162085
+         * always use last as pivot:    164123
+         * always use middle as pivot:  138382
+         */
         br.close();
     }
 }
