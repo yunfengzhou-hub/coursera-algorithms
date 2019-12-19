13b8b3d42ce00df40577c60dcf87a3c81e68bc78
yunfeng zhou <yunfengz@andrew.cmu.edu>
2019-12-18 21:07:49 -0500

    deal with input

--- assignment1-3/QuickSort.java
+++ assignment1-3/QuickSort.java
@@ -1,5 +1,26 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.math.BigInteger;
+
 public class QuickSort {
+    static List<Integer> oriArray;
+    private static BigInteger myIteration(int start, int finish){
+        return BigInteger.valueOf((start+finish)/2);
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello");
+        File file = new File("IntegerArray.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file)); 
+        oriArray = new ArrayList<Integer>();
+        String tmpInput;
+        while((tmpInput = br.readLine()) != null){
+            oriArray.add(Integer.parseInt(tmpInput));
+        }
+        BigInteger invNum = myIteration(0,oriArray.size());
+        System.out.println(invNum);
+        System.out.println(oriArray.size());
+        br.close();
     }
 }
