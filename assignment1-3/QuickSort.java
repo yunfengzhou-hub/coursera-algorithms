d392ed583bc7ab7a9f0dbec8c533676759ba4eb4
yunfeng zhou <yunfengz@andrew.cmu.edu>
2019-12-18 21:19:43 -0500

    finish basic quick sort

--- assignment1-3/QuickSort.java
+++ assignment1-3/QuickSort.java
@@ -4,11 +4,31 @@ import java.io.BufferedReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.math.BigInteger;
+import java.util.Collections;
 
 public class QuickSort {
     static List<Integer> oriArray;
-    private static BigInteger myIteration(int start, int finish){
-        return BigInteger.valueOf((start+finish)/2);
+    static BigInteger compCount = new BigInteger("0");
+    private static void choosePivot(int start, int finish){
+        return ;
+    }
+    private static void myIteration(int start, int finish){
+        if(finish-start<2){
+            return ;
+        }
+        compCount = compCount.add(BigInteger.valueOf(finish-start-1));
+        choosePivot(start,finish);
+        int i,j=start,pivot=oriArray.get(start);
+        for(i=start+1;i<finish;i++){
+            if(oriArray.get(i)<pivot){
+                j++;
+                Collections.swap(oriArray,i,j);
+            }
+        }
+        Collections.swap(oriArray,start,j);
+        myIteration(start, j);
+        myIteration(j+1, finish);
+        return ;
     }
     public static void main(String[] args) throws Exception {
         File file = new File("IntegerArray.txt");
@@ -18,9 +38,9 @@ public class QuickSort {
         while((tmpInput = br.readLine()) != null){
             oriArray.add(Integer.parseInt(tmpInput));
         }
-        BigInteger invNum = myIteration(0,oriArray.size());
-        System.out.println(invNum);
+        myIteration(0,oriArray.size());
         System.out.println(oriArray.size());
+        System.out.println(oriArray.subList(0, 10));
         br.close();
     }
 }
