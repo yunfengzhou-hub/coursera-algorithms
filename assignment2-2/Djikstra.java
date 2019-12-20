65c1495f27f3ce963b0c35e41acf1e8c9d7c4884
yunfeng zhou <yunfengz@andrew.cmu.edu>
2019-12-20 18:39:02 -0500

    alter form to edge list

--- assignment2-2/Djikstra.java
+++ assignment2-2/Djikstra.java
@@ -1,9 +1,11 @@
 import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.util.*;
 
 public class Djikstra{
-    static Integer[][] adjMatrix;
+    // static Integer[][] adjMatrix;
+    static List<List<Integer>> edge;
     static Integer vertNum = 200;
     private static void readFile() throws Exception{
         File file = new File("dijkstraData.txt");
@@ -18,14 +20,17 @@ public class Djikstra{
             v1 = Integer.parseInt(tmpInput[0]);
             for(i=1;i<tmpInput.length;i++){
                 tmpPair = tmpInput[i].split(",");
-                adjMatrix[v1][Integer.parseInt(tmpPair[0])] = Integer.parseInt(tmpPair[1]);
+                // adjMatrix[v1][Integer.parseInt(tmpPair[0])] = Integer.parseInt(tmpPair[1]);
+                edge.add(Arrays.asList(v1,Integer.parseInt(tmpPair[0]),Integer.parseInt(tmpPair[1])));
             }
         }
         br.close();
     }
     public static void main(String[] argv) throws Exception{
-        adjMatrix = new Integer[vertNum+1][vertNum+1];
+        // adjMatrix = new Integer[vertNum+1][vertNum+1];
+        edge = new ArrayList<List<Integer>>();
         readFile();
-        System.out.println(adjMatrix[1][80]);
+        // System.out.println(adjMatrix[1][80]);
+        System.out.println(edge.size());
     }
 } 
