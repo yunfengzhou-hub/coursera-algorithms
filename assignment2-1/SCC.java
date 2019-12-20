1a638a46b5289a53e32a1b1540d8417737cb3836
yunfeng zhou <yunfengz@andrew.cmu.edu>
2019-12-20 18:01:55 -0500

    find when stack will explode

--- assignment2-1/SCC.java
+++ assignment2-1/SCC.java
@@ -7,12 +7,16 @@ import java.util.*;
 public class SCC{
     static int vertNum = 875714;
     static Map<Integer,List<Integer>> adjList, adjListRev;
+    static Integer t,s;
+    static Map<Integer,Integer> vertOrder,vertLeader,strongComponents;
+    static Set<Integer> explored;
     private static void readFile() throws Exception{
         File file = new File("SCC.txt");
         BufferedReader br = new BufferedReader(new FileReader(file)); 
         String tmpLine;
         String[] tmpInput;
         int v1,v2;
+        Integer count = 1000000;
 
         adjList = new HashMap<Integer,List<Integer>>();
 
@@ -29,6 +33,10 @@ public class SCC{
                 adjList.get(v1).add(v2);
             }else{
                 adjList.put(v1,new ArrayList<>(Arrays.asList(v2)));
+            }
+            count --;
+            if(count<=0){
+                break;
             }
         }
         br.close();
@@ -50,13 +58,47 @@ public class SCC{
             }
         }
     }
+    private static void DFS(Map<Integer,List<Integer>> graph,Integer v){
+        try{
+            Integer i;
+            explored.add(v);
+            // vertLeader.put(v,s);
+            if(graph.containsKey(v)){
+                List<Integer> tmpList = graph.get(v);
+                for(i=0;i<tmpList.size();i++){
+                    if(!explored.contains(tmpList.get(i))){
+                        DFS(graph,tmpList.get(i));
+                    }
+                }
+            }
+            // t ++;
+            // vertOrder.put(v,t);
+        }catch(Exception e){
+            System.out.println(e);
+            System.exit(-1);
+        }
+        
+    }
     public static void main(String[] args) throws Exception{
         readFile();
         createRev();
 
-        for (Map.Entry entry : adjListRev.entrySet()) {
-            System.out.println(entry.getKey() + ", " + entry.getValue());
+        Integer v1;
+        vertOrder = new HashMap<Integer,Integer>();
+        vertLeader = new HashMap<Integer,Integer>();
+        explored = new HashSet<Integer>();
+        t = 0;
+        for (Map.Entry<Integer,List<Integer>> entry : adjListRev.entrySet()) {
+            v1 = entry.getKey();
+            if(!explored.contains(v1)){
+                s = v1;
+                DFS(adjListRev,v1);
+            }
         }
+
+        // for (Map.Entry entry : adjListRev.entrySet()) {
+        //     System.out.println(entry.getKey() + ", " + entry.getValue());
+        // }
 
     }
 }
