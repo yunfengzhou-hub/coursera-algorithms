93018573ee7687bd66954fbc6efcc667657537ef
yunfeng zhou <yunfengz@andrew.cmu.edu>
2019-12-20 11:01:14 -0500

    deal with input

--- assignment2-1/SCC.java
+++ assignment2-1/SCC.java
@@ -1,5 +1,19 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
 public class SCC{
-    public static void main(String[] args){
-        System.out.println("hello");
+    public static void main(String[] args) throws Exception{
+        File file = new File("SCC.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file)); 
+        String tmpLine;
+        String[] tmpInput;
+        int v1,v2,i;
+        while((tmpLine = br.readLine()) != null){
+            tmpInput = tmpLine.split(" ");
+            v1 = Integer.parseInt(tmpInput[0]);
+            v2 = Integer.parseInt(tmpInput[1]);
+        }
+        br.close();
     }
 }
