ff723134193a4ec3a3fd2dbc52af67da84658ab9
yunfeng zhou <yunfengz@andrew.cmu.edu>
2019-12-21 16:56:57 -0500

    finish 2 sum brute force

--- assignment2-4/Sum2.java
+++ assignment2-4/Sum2.java
@@ -3,11 +3,11 @@ import java.math.BigInteger;
 import java.util.*;
 
 public class Sum2{
-    private static List<BigInteger> readFile(String filename) throws Exception{
+    private static Set<BigInteger> readFile(String filename) throws Exception{
         File file = new File(filename);
         BufferedReader br = new BufferedReader(new FileReader(file)); 
         String tmpLine;
-        List<BigInteger> number = new ArrayList<BigInteger>();
+        Set<BigInteger> number = new HashSet<BigInteger>();
 
         while((tmpLine = br.readLine()) != null){
             number.add(new BigInteger(tmpLine));
@@ -16,7 +16,22 @@ public class Sum2{
         return number;
     }
     public static void main(String[] args) throws Exception{
-        List<BigInteger> number = readFile("2Sum.txt");
-        System.out.println(Arrays.toString(number.toArray()));
+        Set<BigInteger> number = readFile("2Sum.txt");
+        Set<BigInteger> result = new HashSet<BigInteger>();
+        BigInteger i,j=BigInteger.ZERO,k=BigInteger.ZERO,h=new BigInteger("1000");
+        BigInteger[] thresh = {new BigInteger("-10000"),new BigInteger("10000")};
+        for(BigInteger entry:number){
+            for(i=thresh[0];i.compareTo(thresh[1])<=0;i=i.add(BigInteger.ONE)){
+                if(!result.contains(i) && number.contains(i.subtract(entry)) && entry.compareTo(i.subtract(entry))!=0){
+                    result.add(i);
+                }
+            }
+            j=j.add(BigInteger.ONE);
+            if(j.mod(h).compareTo(BigInteger.ZERO)==0){
+                k=k.add(BigInteger.ONE);
+                System.out.println(k);
+            }
+        }
+        System.out.println(result.size());
     }
 }
