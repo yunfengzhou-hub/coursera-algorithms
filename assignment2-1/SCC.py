ee2916e9279490fdd758b5b57694628107653aa2
yunfeng zhou <yunfengz@andrew.cmu.edu>
2019-12-21 17:58:44 -0500

    tried python version; still problematic

--- assignment2-1/SCC.py
+++ assignment2-1/SCC.py
@@ -1,5 +1,19 @@
-f = open("SCC.txt")
+# expected answer: 434821, 968, 459, 313, 211
+
+t = 0
+s = ""
+
+explored = {}
 adjList = {}
+
+def DFS(i):
+    explored[i] = True
+    if i in adjList:
+        for j in adjList[i]:
+            if j not in explored:
+                DFS(j)
+
+f = open("SCC.txt")
 for line in f.readlines():
     v1,v2 = line.split()
     if v1 in adjList:
@@ -7,4 +21,12 @@ for line in f.readlines():
     else:
         adjList[v1]=[v2]
 
-print(adjList["1"])
+# print(adjList["1"])
+
+
+for i in range(875714,0,-1):
+    if str(i) not in explored:
+        s = str(i)
+        DFS(str(i))
+
+print(len(explored))
